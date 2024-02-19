023516d5d613432380df25616be8495a5271f911
Mukesh <mukeshtopcoder@gmail.com>
2024-02-19 23:20:47 +0530

    Update Programs

---
+++ AddTwoNumbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class AddTwoNumbers{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int a,b,c;
+		System.out.print("Enter A Numbers : ");
+		a = sc.nextInt();
+		System.out.print("Enter B Numbers : ");
+		b = sc.nextInt();
+		c = a+b;
+		System.out.print("Addition : "+c);
+	}
+}
