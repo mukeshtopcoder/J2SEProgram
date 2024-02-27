f58ebeca3331c1f59a3207472f1273176bd7ba26
Mukesh <mukeshtopcoder@gmail.com>
2024-02-27 22:35:15 +0530

    Update Programs

--- JavaStringPrograms/LongestRepeatingString.java
+++ JavaStringPrograms/LongestRepeatingString.java
@@ -1,6 +1,23 @@
-// Longest Repeating String
+// let String = helloindiahelloworld
+// In this String 'hello' is longest repeating String
 class LongestRepeatingString{
+	public static String LongString(String s,String r){
+		int len = Math.min(s.length(),r.length());
+		for(int i=0;i<len;i++){
+			if(s.charAt(i)!=r.charAt(i))
+				return s.substring(0,i);
+		}
+		return s.substring(0,len);
+	}
 	public static void main(String[] args){
-		String str = "abbcdbbcc";
+		String str = "amanismybrotheramanisagoodboy";
+		String lrs = "";
+		int l = str.length();
+		for(int i=0;i<l;i++){
+			String temp = LongString(str.substring(0,l),str.substring(i+1,l));
+			if(temp.length()>lrs.length())
+				lrs = temp;
+		}
+		System.out.print("Longest String : "+lrs);
 	}
 }
---
+++ JavaStringPrograms/PermutationOfString.java
@@ -0,0 +1,11 @@
+// Find All Permutation Of A String
+// ABC : ABC , CBA , BCA , ACB , CAB , BAC
+public class PermutationOfString{
+	public static void Permutation(){
+		
+	}
+	public static void main(String[] args){
+		String str = "ABCD";
+		
+	}
+}
